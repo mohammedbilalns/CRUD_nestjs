bc8953867893223255dd0c91d35f332f3adcf800
Mohammed Bilal <bilalnsmuhammed@gmail.com>
2025-12-23 00:08:51 +0530

    fix : validation error on update

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,25 +1,21 @@
 
-import { Optional } from "@nestjs/common"
-import { IsNotEmpty,IsString,  MaxLength,  MinLength} from "class-validator"
+import { IsOptional, IsString,  MaxLength,  MinLength} from "class-validator"
 
 export class UpdatePostDto{
 
-  @Optional()
-  @IsNotEmpty({message: "Title is required"})
+  @IsOptional()
   @IsString({message:'Title must be a string '})
   @MinLength(3,{message: 'Title must be atleast 3 chars long'})
   @MaxLength(50, {message:'Title can not be longer than 50 chars'})
   title: string
 
 
-  @Optional()
-  @IsNotEmpty({message: "Content is required"})
+  @IsOptional()
   @IsString({message:'Content must be a string '})
   @MinLength(3,{message: 'Content must be atleast 3 chars long'})
   content: string
 
-  @Optional()
-  @IsNotEmpty({message: "Author is required"})
+  @IsOptional()
   @IsString({message:'Author name must be a string '})
   @MinLength(3,{message: 'Author name must be atleast 3 chars long'})
   @MaxLength(50, {message:'Author can not be longer than 50 chars'})
