6439ff02f46acd481e25d719caf572c8ba1a3535
Mohammed Bilal <bilalnsmuhammed@gmail.com>
2025-12-23 13:55:34 +0530

    in memory cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { Post } from './posts/entities/post.entity';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/entities/user.entity';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -16,6 +17,11 @@ import { ThrottlerModule } from '@nestjs/throttler';
         limit:5
       }
     ]),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 30000,
+      max: 100
+    }),
     TypeOrmModule.forRoot({
       type: "postgres",
       host:'localhost',
